c40088bb2c90971e4faeb51d19690113ecf60ca9
liuruizhou <liuruizhou@bilibili.com>
2022-06-08 11:37:46 +0800

    update lfu

--- lru-lfu/lfu_test.go
+++ lru-lfu/lfu_test.go
@@ -5,12 +5,7 @@ import (
 )
 
 func NewOneLfu() LFUCache {
-	return NewLfu(6)
-}
-
-func TestOneLfu(t *testing.T) {
-
-	lfu := NewOneLfu()
+	lfu := NewLfu(6)
 	lfu.Put(1, 101)
 
 	lfu.Put(2, 102)
@@ -31,11 +26,17 @@ func TestOneLfu(t *testing.T) {
 	lfu.Put(333, 333)
 	lfu.Put(333, 333)
 
-	lfu.Put(99, 999)
-	lfu.Put(99, 999)
-	lfu.Put(99, 999)
+	return lfu
+}
+
+func TestOneLfu(t *testing.T) {
+
+	lfu := NewOneLfu()
+	// list root node is nil 拒绝访问的
 
 	t.Errorf("lfu")
 	t.Logf("lfu is %#v", lfu.lists[2])
+	t.Logf("lfu is %#v", lfu.lists[2].Front().Prev())
+	t.Logf("lfu is %#v", lfu.lists[2].Back().Next())
 
 }
